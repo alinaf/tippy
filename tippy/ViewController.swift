213828df60d57699015c696d7a0bfd9924799ead
Alina Abidi <alinaf@fb.com>
2017-06-20 15:44:08 -0700

    Added functionality to calculator: tips and total are now displayed when bill value is changed or tip control is toggled

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        // If the double of the user's value equals nil (i.e. they don't enter a number), return the default value of 0
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
 }
 
